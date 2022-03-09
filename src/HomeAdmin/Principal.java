402c284445506fda10cfa9dcdc328e16e8959efb
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2022-03-09 11:41:16 -0500

    fix 0.1

--- src/HomeAdmin/Principal.java
+++ src/HomeAdmin/Principal.java
@@ -17,6 +17,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        HomeAdmin homeAdmin = new HomeAdmin();
     }
     
 }
