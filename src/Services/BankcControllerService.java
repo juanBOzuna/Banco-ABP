1638dede3b62a5b31281c052407c81d1111b2434
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2022-03-26 10:26:46 -0500

    add bank controller service

---
+++ src/Services/BankcControllerService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Objects.BankModel;
+
+/**
+ *
+ * @author juan barraza
+ */
+public class BankcControllerService {
+     public static BankModel get(int id) {
+        return BankModel.index(id);
+    }
+
+    public static Boolean delete(int id) {
+        return BankModel.delete(id);
+    }
+
+    public static BankModel update(BankModel bank) {
+        return BankModel.Update(bank);
+    }
+
+    public static BankModel create(BankModel bank) {
+        return BankModel.Create(bank);
+    }
+}
