b21ec83ca4190518574476dba9355837540e7f53
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2022-03-26 10:14:30 -0500

    add account current service

---
+++ src/Services/AccountCurrentService.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Objects.AccountCurrentModel;
+
+/**
+ *
+ * @author juan barraza
+ */
+public class AccountCurrentService {
+
+    public static AccountCurrentModel get(int id) {
+
+        return null;
+    }
+
+    public static Boolean delete(int id) {
+
+        return null;
+    }
+
+    public static AccountCurrentModel update(AccountCurrentModel bank) {
+
+        return null;
+    }
+
+    public static AccountCurrentModel create(AccountCurrentModel bank) {
+
+        return null;
+    }
+}
